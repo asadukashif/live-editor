52de3e1bf415c906aa6d9dc1c83f1f5f2b62e5a7
Asadullah Kashif <s.asad18k@gmail.com>
2021-03-19 23:08:49 +0500

    Socket.io initialized and tested

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application } from "express";
 import nunjucks from "nunjucks";
 import dotenv from "dotenv";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 
 import HomeRoute from "./routes/HomeRoute";
 
@@ -8,10 +10,14 @@ dotenv.config();
 const PORT: number = parseInt(<string>process.env.PORT) || 3000;
 
 const app: Application = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {});
+
 app.set("view engine", "html");
+app.use(express.static(__dirname + "/static/"));
 
 // Nunjucks Settings
-nunjucks.configure(["src/views/", "src/views/static/"], {
+nunjucks.configure(["src/views/", "src/static/"], {
   autoescape: false,
   express: app,
 });
@@ -19,4 +25,11 @@ nunjucks.configure(["src/views/", "src/views/static/"], {
 // Routes
 app.use("/", HomeRoute);
 
-app.listen(PORT, () => console.log(`Server up at http://localhost:${PORT}/`));
+/* SocketIO Jazz */
+io.on("connection", (socket: Socket) => {
+  console.log("Connected to", socket.id);
+  socket.emit("welcome", { message: "Welcome to the server" });
+});
+
+httpServer.listen(PORT);
+console.log("Server is up at http://localhost:3000/");
