5eb9080c897c4c6599b47a716c7648087b74114e
Asadullah Kashif <s.asad18k@gmail.com>
2021-05-25 15:46:03 +0500

    clean slate 💯

---
+++ frontend/src/App.js
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="App">Hello</div>;
+};
+
+export default App;
--- src/routes/HomeRoute.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response, Router } from "express";
-
-const router: Router = express.Router();
-
-router.use("/", (req: Request, res: Response) => {
-  res.render("home/home.html", { title: "Home Page" });
-});
-
-export default router;
