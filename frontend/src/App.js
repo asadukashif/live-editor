a5ede8fe570ff9ac90a63194b9b7764585e6db71
Asadullah Kashif <s.asad18k@gmail.com>
2021-05-26 14:26:45 +0500

    working now, will refactor in a bit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,25 @@
-const App = () => {
-  return <div className="App">Hello</div>;
-};
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
+import TextEditor from "./components/Editor";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to={`/documents/${uuidV4()}`} />
+        </Route>
+        <Route path="/documents/:id">
+          <TextEditor />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
 
 export default App;
