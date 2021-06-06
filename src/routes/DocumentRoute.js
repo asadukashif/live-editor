e04e5b0d876b5d8345c14ab2c02c75e8bb3e8281
Asadullah Kashif <s.asad18k@gmail.com>
2021-06-06 06:13:10 +0500

    minor refactoring

--- src/routes/DocumentRoute.js
+++ src/routes/DocumentRoute.js
@@ -2,22 +2,20 @@ const router = require("express").Router();
 const { ensureDoc, createDoc } = require("../middleware/document");
 const { v4 } = require("uuid");
 
-router.get("/new/",(req, res) => {
+router.get("/new/", (req, res) => {
   res.render("create_new.html");
 });
 
-router.get("/new/:lang", createDoc ,(req, res) => {
+router.get("/new/:lang", createDoc, (req, res) => {
   res.redirect(`/document/${req.params.docid}`);
 });
 
-router.get(
-  "/:id/", ensureDoc, (req, res) => {
-    if (req.params.docFound) {
-      return res.render("document.html");
-    } else {
-      return res.send("Could not find the document");
-    }
+router.get("/:id/", ensureDoc, (req, res) => {
+  if (req.params.docFound) {
+    return res.render("document.html");
+  } else {
+    return res.send("Could not find the document");
   }
-);
+});
 
 module.exports = router;
