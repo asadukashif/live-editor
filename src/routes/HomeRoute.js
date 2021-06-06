bab0ed18e97809d54634b54dffd4072501a41dea
green-mint <saad.521509@gmail.com>
2021-06-06 15:39:51 +0500

    query user docs when he visits his page

--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -1,8 +1,25 @@
 const router = require("express").Router();
+const { ensureAuth } = require("../middleware/auth")
+const Document = require("../models/Document")
 
 router.get("/", (req, res) => {
   console.log(req.user);
   res.render("home.html");
 });
+
+router.get("/user/:id", ensureAuth, (req, res) => {
+  if (req.user != req.params.id) {
+    return res.send("You are not allowed to view this page")
+  }
+  
+  Document.find({
+    owner: req.params.id,
+  }, (err, docs) => {
+    console.log(err);
+    console.log(docs);
+  })
+
+  res.render("document_new.html")
+})
 
 module.exports = router;
