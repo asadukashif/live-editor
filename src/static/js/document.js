e3128ff8208cb963a24d9bdf93ef3938a44f5ad5
Asadullah Kashif <s.asad18k@gmail.com>
2021-06-06 06:13:27 +0500

    added support for a remote execution engine

--- src/static/js/document.js
+++ src/static/js/document.js
@@ -1,25 +1,56 @@
-let WebSocket = require("reconnecting-websocket");
-let ShareDB = require("sharedb/lib/client");
-let CodeMirror = require("codemirror");
+const WebSocket = require("reconnecting-websocket");
+const ShareDB = require("sharedb/lib/client");
+const CodeMirror = require("codemirror");
 require("codemirror/mode/javascript/javascript");
-// require('codemirror/mode/')
-let ShareDBCodeMirror = require("./sharedb-codemirror");
-let { langMap } = require("../../config/langMap");
-let debug = true;
+const ShareDBCodeMirror = require("./sharedb-codemirror");
+const { langMap } = require("../../config/langMap");
 let ws, connection, codeMirror, shareDBCodeMirror;
-let docid = window.location.href.split("/")[4];
-let language = langMap[docid.slice(0, 4)];
+const docid = window.location.href.split("/")[4];
+const language = langMap[docid.slice(0, 4)];
+const Split = require("split.js");
+
+const DEBUG = false;
+const MODES = {
+  python: "text/x-python",
+  "node-js": "text/javascript",
+  cpp: "text/x-c++src",
+};
+const EXTS = {
+  python: ".py",
+  "node-js": ".js",
+  cpp: ".cpp",
+};
+const VERSIONS = {
+  python: "3.9.4",
+  "node-js": "15.10.0",
+  cpp: "10.2.0",
+};
+
 console.log(language);
 window.onload = event => {
   ws = new WebSocket("ws://" + window.location.host);
   connection = new ShareDB.Connection(ws);
+
+  switch (language) {
+    case "cpp":
+      require("codemirror/mode/clike/clike");
+      break;
+    case "node-js":
+      require("codemirror/mode/javascript/javascript");
+      break;
+    case "python":
+      require("codemirror/mode/python/python");
+      break;
+  }
+
   codeMirror = new CodeMirror(document.getElementById("textarea"), {
     lineNumbers: true,
+    tabSize: 2,
     theme: "material-darker",
-    mode: "text/x-csrc",
+    mode: MODES[language],
   });
   shareDBCodeMirror = new ShareDBCodeMirror(codeMirror, {
-    verbose: debug,
+    verbose: DEBUG,
     key: "content",
   });
   console.log(docid);
@@ -29,27 +60,54 @@ window.onload = event => {
       console.error(error);
       return;
     }
-    codeMirror.setOption("mode", "javascript");
+    codeMirror.setOption("mode", MODES[language]);
   });
 };
-let runButton = document.getElementById("run-code");
-runButton.addEventListener("click", () => {
-  console.log(codeMirror);
-  console.log();
-  const data = { code: codeMirror.getValue() };
-  console.log(data);
-  fetch("/run", {
-    method: "POST", // or 'PUT'
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log("Success:", data);
+
+document.addEventListener("DOMContentLoaded", async () => {
+  Split(["#textarea", "#terminal"], {
+    direction: "vertical",
+  });
+
+  const terminalOutput = document.getElementById("terminal-output");
+
+  const runButton = document.getElementById("run-code");
+  runButton.addEventListener("click", () => {
+    const code = codeMirror.getValue();
+
+    terminalOutput.classList.remove("text-danger");
+    terminalOutput.innerHTML = "Compiling...";
+
+    fetch("https://emkc.org/api/v2/piston/execute", {
+      method: "POST",
+      body: JSON.stringify({
+        version: VERSIONS[language],
+        language,
+        files: [
+          {
+            name: `${new Date().toISOString()}${EXTS[language]}`,
+            content: code,
+          },
+        ],
+        stdin: "",
+        args: [],
+        compile_timeout: 10000,
+        run_timeout: 3000,
+        compile_memory_limit: -1,
+        run_memory_limit: -1,
+      }),
     })
-    .catch(error => {
-      console.error("Error:", error);
-    });
+      .then(response => response.json())
+      .then(data => {
+        let output = "";
+        if (!data.run.stderr) {
+          output = data.run.stdout;
+        } else {
+          terminalOutput.classList.add("text-danger");
+          output = data.run.stderr;
+        }
+        terminalOutput.innerHTML =
+          output + `\n\nProgram finished with exit code ${data.run.code}`;
+      });
+  });
 });
