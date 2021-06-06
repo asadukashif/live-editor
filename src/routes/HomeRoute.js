2d1f1158d9b9a4f97649b4a729dc9c9b066f4fdf
Asadullah Kashif <s.asad18k@gmail.com>
2021-06-06 06:14:38 +0500

    removed user from being passed to the fronend

--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -2,9 +2,7 @@ const router = require("express").Router();
 
 router.get("/", (req, res) => {
   console.log(req.user);
-  res.render("home.html", {
-    user: req.user
-  });
+  res.render("home.html");
 });
 
 module.exports = router;
