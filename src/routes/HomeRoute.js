ffe827b9ba62140d6381e04f0b02277290b5e7b2
Asadullah Kashif <s.asad18k@gmail.com>
2021-06-09 03:07:42 +0500

    changed the name of the template

--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const { ensureAuth } = require("../middleware/auth")
-const Document = require("../models/Document")
+const { ensureAuth } = require("../middleware/auth");
+const Document = require("../models/Document");
 
 router.get("/", (req, res) => {
   console.log(req.user);
@@ -9,20 +9,24 @@ router.get("/", (req, res) => {
 
 router.get("/users/:id", ensureAuth, (req, res) => {
   if (req.user._id != req.params.id) {
-    return res.send("You are not allowed to view this page")
+    return res.send("You are not allowed to view this page");
   }
-  
-  Document.find({
-    owner: req.params.id,
-  }, (err, docs) => {
-    if (err) {
-      return res.send("We are facing some technical issues! Apologies for any inconvience");
-    }
-    return res.render("document_new.html", {
-      documents: docs,
-    })
-  })
 
-})
+  Document.find(
+    {
+      owner: req.params.id,
+    },
+    (err, docs) => {
+      if (err) {
+        return res.send(
+          "We are facing some technical issues! Apologies for any inconvience"
+        );
+      }
+      return res.render("user_profile.html", {
+        documents: docs,
+      });
+    }
+  );
+});
 
 module.exports = router;
